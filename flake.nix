a397752317e311d2ef185d1d4e151047d2bfaece
XanderTheDev <forxanderonly@gmail.com>
2024-08-28 00:26:01 +0200

    weird names nixos...

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 		
 		home-manager.nixosModules.home-manager {
 			home-manager.useGlobalPkgs = true;
-			home-manager.useUserPkgs = true;
+			home-manager.useUserPackages = true;
 			
 			home-manager.users.xander = import ./home.nix;
 		}
