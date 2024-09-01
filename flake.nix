099a6d4cf52b1c59391a3aabd4c3fd4d64e64afc
XanderTheDev <forxanderonly@gmail.com>
2024-09-01 12:14:19 +0200

    changed ags setup

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
 	url = "github:nix-community/home-manager?ref=release-24.05";
 	inputs.nixpkgs.follows = "nixpkgs";
     };
+    ags = {
+	url = "github:Aylur/ags";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ { nixpkgs, home-manager, ... }:
