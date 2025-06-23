c4825a114551b9b789ee7ddce097993c92a9f8aa
XanderTheDev <forxanderonly@gmai.com>
2025-06-23 14:38:34 +0200

    Changed theme to spinning nixos logo

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, inputs, ... }:
-
+{ config, lib, pkgs, inputs, ... }: 
 {
   
   imports =
@@ -26,7 +25,7 @@
           # selected_themes = [ "rings" ];
         # })
       # ];
-      theme = lib.mkForce "bgrt";
+      #theme = lib.mkForce "bgrt";
     };
 
     # Enable "Silent boot"
